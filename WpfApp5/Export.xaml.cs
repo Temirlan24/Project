4571a622bd80a5aaf7746849a5c7fec852019dfb
danil355 <52044554+danil355@users.noreply.github.com>
2020-03-30 18:02:07 +0600

    Init

--- WpfApp5/Export.xaml.cs
+++ WpfApp5/Export.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
+using javax.print;
+using Magnum.FileSystem;
+using System.Diagnostics;
 using System.IO;
 using System.Windows;
+using Xceed.Words.NET;
 using Document = iTextSharp.text.Document;
 using Word = Microsoft.Office.Interop.Word;
+
 namespace WpfApp5
 {
     /// <summary>
@@ -11,47 +16,110 @@ namespace WpfApp5
     /// </summary>
     public partial class Export : Window
     {
+        RecipeWindow recipeWindow = new RecipeWindow();
+        MainWindow mainWindow = new MainWindow();
         private string FileName = @"D:\text.docx";
         public Export()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnDoc_Click(object sender, RoutedEventArgs e)
         {
-            var objWord = new Word.Application();
-            objWord.Visible = false;
-            try
-            {
-                Word.Document objDoc = objWord.Documents.Add();
-                var doc = objWord.Documents.Open(FileName);
-                Word.Paragraph objPar;
-                objPar = objDoc.Paragraphs.Add();
-                objPar.Range.Text = "Danildanildanil";
-                doc.SaveAs(@"D:\result.docx");
-                doc.Close();
-            }
-            catch
-            {
-                MessageBox.Show("Произошла ошибка");
-            }
+            //var objWord = new Word.Application();
+            //objWord.Visible = false;
+            //try
+            //{
+            //    Word.Document objDoc = objWord.Documents.Add();
+            //    var doc = objWord.Documents.Open(FileName);
+            //    Word.Paragraph objPar;
+            //    objPar = objDoc.Paragraphs.Add();
+            //    objPar.Range.Text = "Danildanildanil";
+            //    doc.SaveAs(@"D:\result.docx");
+            //    doc.Close();
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Произошла ошибка");
+            //}
 
-            finally
-            {
-                objWord.Quit();
-            }
+            //finally
+            //{
+            //    objWord.Quit();
+            //}
+
+            string FileName = "danilllllll.doc";
+            var doc = Doc.Create(FileName);
+            doc.InsertParagraph("Hi EveryOne");
+
+
+            doc.Save();
+
+            Process.Start("WpfApp5.EXE", FileName);
+
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnPdf_Click_1(object sender, RoutedEventArgs e)
         {
-
             Document doc = new Document();
             PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Test.pdf", FileMode.Create));
             doc.Open();
-            Paragraph par = new Paragraph("Me name is Danil");
+            Paragraph par = new Paragraph(mainWindow.ContentStringFormat);
             doc.Add(par);
-            doc.Close();
             MessageBox.Show("Ok");
+            doc.Close();
         }
+
+        private void btnDocX_Click(object sender, RoutedEventArgs e)
+        {
+            string FileName = "danilllllll.docx";
+            var doc = DocX.Create(FileName);
+            doc.InsertParagraph("Hi EveryOne");
+
+
+            doc.Save();
+
+            Process.Start("WpfApp5.EXE", FileName);
+        }
+
+
+
     }
 }
+
+
+/*01
+private void button1_Click(object sender, RoutedEventArgs e)<font></font>
+02
+        {<font></font>
+03
+            //Create New Word<font></font>
+04
+            Document doc = new Document();<font></font>
+05
+            //Add Section<font></font>
+06
+            Section section = doc.AddSection();<font></font>
+07
+            //Add Paragraph<font></font>
+08
+            Paragraph Para = section.AddParagraph();<font></font>
+09
+            //Append Text<font></font>
+10
+            Para.AppendText("Hello! "<font></font>
+11
+            + "I was created by Spire.Doc for WPF, it's a professional .NET Word component "<font></font>
+12
+            + "which enables developers to perform a large range of tasks on Word document (such as create, open, write, edit, save and convert "<font></font>
+13
+            + "Word document) without installing Microsoft Office and any other third-party tools on system.");<font></font>
+14
+            //Save and launch<font></font>
+15
+            doc.SaveToFile("MyWord.docx", FileFormat.Docx);<font></font><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+16
+            System.Diagnostics.Process.Start ( "MyWord.docx");</font></font><font></font><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+17
+        }</font></font><font></font>
+*/
